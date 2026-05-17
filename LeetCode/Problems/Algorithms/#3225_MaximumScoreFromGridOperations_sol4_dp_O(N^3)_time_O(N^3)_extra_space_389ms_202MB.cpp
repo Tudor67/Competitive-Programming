1e8d3f1292cab1f5fb1ec52e9421d5f57e681122
Tudor Buzu <buzu.tudor67@gmail.com>
2026-05-17 23:46:00 +0300

    added solutions for 5th week of April-LeetCoding-Challenge-2026

---
+++ LeetCode/Problems/Algorithms/#3225_MaximumScoreFromGridOperations_sol4_dp_O(N^3)_time_O(N^3)_extra_space_389ms_202MB.cpp
@@ -0,0 +1,80 @@
+class Solution {
+private:
+    long long getColSum(vector<long long>& suffSum, int firstRow, int lastRow){
+        if(firstRow > lastRow){
+            return 0;
+        }
+        return suffSum[firstRow] - suffSum[lastRow + 1];
+    }
+
+    void maximize(long long& a, long long b){
+        a = max(a, b);
+    }
+
+public:
+    long long maximumScore(vector<vector<int>>& grid) {
+        const int N = grid.size();
+
+        vector<vector<long long>> suffSumOfCol(N, vector<long long>(N + 1));
+        for(int row = N - 1; row >= 0; --row){
+            for(int col = 0; col < N; ++col){
+                suffSumOfCol[col][row] = grid[row][col] + suffSumOfCol[col][row + 1];
+            }
+        }
+
+        // dp[i][r1][r0]: max score considering columns [0 .. i]
+        //                where:
+        //                  - column (i - 1) has its first r1 rows black
+        //                  - column (i)     has its first r0 rows black
+        vector<vector<vector<long long>>> dp(N, vector<vector<long long>>(N + 1, vector<long long>(N + 1)));
+        vector<long long> prevDPMax(N + 1);
+        vector<long long> prevMaxLE(N + 1);
+        vector<long long> prevMaxGE(N + 1);
+
+        for(int i = 1; i < N; ++i){
+            fill(prevDPMax.begin(), prevDPMax.end(), 0);
+            for(int r1 = 0; r1 <= N; ++r1){
+                for(int r2 = 0; r2 <= N; ++r2){
+                    maximize(prevDPMax[r1], dp[i - 1][r2][r1]);
+                }
+            }
+
+            fill(prevMaxLE.begin(), prevMaxLE.end(), 0);
+            fill(prevMaxGE.begin(), prevMaxGE.end(), 0);
+            for(int r1 = 0; r1 <= N; ++r1){
+                for(int r2 = 0; r2 <= N; ++r2){
+                    maximize(prevMaxLE[max(r1, r2)], dp[i - 1][r2][r1]);
+                    maximize(prevMaxGE[max(r1, r2)], dp[i - 1][r2][r1]);
+                }
+            }
+
+            for(int rMax = 1; rMax <= N; ++rMax){
+                maximize(prevMaxLE[rMax], prevMaxLE[rMax - 1]);
+            }
+
+            for(int rMax = N - 1; rMax >= 0; --rMax){
+                maximize(prevMaxGE[rMax], prevMaxGE[rMax + 1]);
+            }
+
+            for(int r0 = 0; r0 <= N; ++r0){
+                for(int r1 = 0; r1 <= N; ++r1){
+                    if(r1 > r0){
+                        maximize(dp[i][r1][r0], prevDPMax[r1] + getColSum(suffSumOfCol[i], r0, r1 - 1));
+                    }else{
+                        maximize(dp[i][r1][r0], prevMaxLE[r1] + getColSum(suffSumOfCol[i - 1], r1, r0 - 1));
+                        maximize(dp[i][r1][r0], prevMaxGE[r0]);
+                    }
+                }
+            }
+        }
+
+        long long res = 0;
+        for(int r0 = 0; r0 <= N; ++r0){
+            for(int r1 = 0; r1 <= N; ++r1){
+                res = max(res, dp[N - 1][r1][r0]);
+            }
+        }
+
+        return res;
+    }
+};
---
+++ LeetCode/Problems/Algorithms/#3742_MaximumPathScoreInAGrid_sol2_dp_O(RxCxK)_time_O(CxK)_extra_space_671ms_119MB.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int maxPathScore(vector<vector<int>>& grid, int k) {
+        const int ROWS = grid.size();
+        const int COLS = grid[0].size();
+        const vector<pair<int, int>> DIRECTIONS = {{1, 0}, {0, 1}};
+
+        // At (row)th iteration:
+        //    dp[row % 2][col][c]: max score to reach (row, col) from (0, 0) with cost c
+        vector<vector<vector<int>>> dp(2, vector<vector<int>>(COLS, vector<int>(k + 1, -1)));
+
+        dp[0][0][0] = 0;
+        for(int row = 0; row < ROWS; ++row){
+            for(int col = 0; col < COLS; ++col){
+                for(int cost = 0; cost <= k; ++cost){
+                    dp[(row + 1) % 2][col][cost] = -1;
+                }
+            }
+            
+            for(int col = 0; col < COLS; ++col){
+                for(int cost = 0; cost <= k; ++cost){
+                    if(dp[row % 2][col][cost] == -1){
+                        continue;
+                    }
+                    for(const auto& [rowDir, colDir]: DIRECTIONS){
+                        int nextRow = row + rowDir;
+                        int nextCol = col + colDir;
+                        if(nextRow < ROWS && nextCol < COLS){
+                            int nextCost = cost + min(1, grid[nextRow][nextCol]);
+                            if(nextCost <= k){
+                                dp[nextRow % 2][nextCol][nextCost] = max(dp[nextRow % 2][nextCol][nextCost],
+                                                                         dp[row % 2][col][cost] + grid[nextRow][nextCol]);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return *max_element(dp[(ROWS - 1) % 2][COLS - 1].begin(), dp[(ROWS - 1) % 2][COLS - 1].end());
+    }
+};
